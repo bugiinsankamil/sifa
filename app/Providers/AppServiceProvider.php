661aa257679019484406e54c1a0c1a836f7074a9
Bugi Insan Kamil <bugiinsankamil@gmail.com>
2026-03-13 10:17:59 +0700

    Add media library configuration file and implement language switch in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\LanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +21,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $languageSwitch) {
+            $languageSwitch
+                ->locales([
+                    'en',
+                    'id',
+                ]);
+        });
     }
 }
