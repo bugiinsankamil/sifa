b5b1ca4e94d2a29b909970a4098a1b7082b47c4a
Bugi Insan Kamil <bugiinsankamil@gmail.com>
2026-03-14 08:08:17 +0700

    Add base model and loc country model with migration
    - Created BaseModel class for common Eloquent model functionality with activity logging.
    - Added LocCountry model extending BaseModel.
    - Implemented migration for loc_countries table with timestamps.
    - Introduced various stubs for generating controllers, requests, notifications, and more.

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class BaseModel extends Model
+{
+    use LogsActivity;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logOnlyDirty();
+    }
+}
