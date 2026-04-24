59511dfdecaf20cecc5be7d859241b25afc157b0
Bugi Insan Kamil <bugiinsankamil@gmail.com>
2026-04-24 14:11:45 +0700

    feat: Update portal view with new color scheme and content for user sections

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -47,13 +47,13 @@
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-20">
                 <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Welcome to SIFA!</h1>
-                <h2 class="text-xs text-yellow-500 tracking-widest font-medium title-font mb-1">Sistem Informasi Fathona</h2>
+                <h2 class="text-xs text-orange-500 tracking-widest font-medium title-font mb-1">Sistem Informasi Fathona</h2>
             </div>
             <div class="flex flex-wrap -m-4">
                 <div class="p-4 md:w-1/3">
                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                         <div class="flex items-center mb-3">
-                            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-yellow-500 text-white flex-shrink-0">
+                            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-orange-500 text-white flex-shrink-0">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                                 </svg>
@@ -61,9 +61,10 @@
                             <h2 class="text-gray-900 text-lg title-font font-medium">SIFA for Management</h2>
                         </div>
                         <div class="flex-grow">
-                            <p class="leading-relaxed text-base">Sistem informasi untuk manajemen Fathona</p>
-                            <a class="mt-3 text-yellow-500 inline-flex items-center" href="/school">Learn More
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <p class="leading-relaxed text-base">Sistem Informasi Fathona untuk Manajemen Sekolah</p>
+                            <a href="/school" class="flex mx-auto mt-8 text-white bg-orange-500 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg">
+                                <p>Sign In</p>
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mt-0.5 w-6 h-6 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
@@ -73,18 +74,19 @@
                 <div class="p-4 md:w-1/3">
                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                         <div class="flex items-center mb-3">
-                            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-yellow-500 text-white flex-shrink-0">
+                            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-orange-500 text-white flex-shrink-0">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                                     <circle cx="12" cy="7" r="4"></circle>
                                 </svg>
                             </div>
-                            <h2 class="text-gray-900 text-lg title-font font-medium">The Catalyzer</h2>
+                            <h2 class="text-gray-900 text-lg title-font font-medium">SIFA for Teacher</h2>
                         </div>
                         <div class="flex-grow">
-                            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                            <a class="mt-3 text-yellow-500 inline-flex items-center">Learn More
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <p class="leading-relaxed text-base">Sistem Informasi Fathona untuk Guru</p>
+                            <a class="flex mx-auto mt-8 text-white bg-orange-500 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg">
+                                <p>Sign In</p>
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mt-0.5 w-6 h-6 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
@@ -94,19 +96,20 @@
                 <div class="p-4 md:w-1/3">
                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                         <div class="flex items-center mb-3">
-                            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-yellow-500 text-white flex-shrink-0">
+                            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-orange-500 text-white flex-shrink-0">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                     <circle cx="6" cy="6" r="3"></circle>
                                     <circle cx="6" cy="18" r="3"></circle>
                                     <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
                                 </svg>
                             </div>
-                            <h2 class="text-gray-900 text-lg title-font font-medium">Neptune</h2>
+                            <h2 class="text-gray-900 text-lg title-font font-medium">SIFA for Parents</h2>
                         </div>
                         <div class="flex-grow">
-                            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                            <a class="mt-3 text-yellow-500 inline-flex items-center">Learn More
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <p class="leading-relaxed text-base">Sistem Informasi Fathona untuk Orang Tua</p>
+                            <a class="flex mx-auto mt-8 text-white bg-orange-500 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg">
+                                <p>Sign In</p>
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mt-0.5 w-6 h-6 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
